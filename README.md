ca8044994cdd1e60c8ef2cbc2c8c8854f54cf645
Dfletch <plebeianpony@gmail.com>
2023-03-12 15:11:03 -0500

    restarted project because its confusing.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio-2023
-An updated portfolio to store and display my work.
---
+++ portfolio-2023/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+   <Routes>
+      <Routes path="/" element={<Layout />} />
+   </Routes>
+  );
+}
+
+export default App;
